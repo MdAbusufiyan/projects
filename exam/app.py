59ef36ed677de39315637eba5c717d2944e0f3f1
mdpatel <mdpatel@example.com>
2026-08-13 22:12:26 +0530

    patch

--- exam/app.py
+++ exam/app.py
@@ -85,4 +85,10 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app, host="127.0.0.1", port=8520, debug=False)
+    socketio.run(
+        app,
+        host="127.0.0.1",
+        port=8520,
+        debug=False,
+        allow_unsafe_werkzeug=True,
+    )
